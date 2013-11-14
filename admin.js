9524b0733f3e718389c5cfd6d189d6d64bc65f0b
MarkTFerry <mtferry@asu.edu>
2013-11-14 13:02:37 -0700

    Added Admin Portal

---
+++ admin.js
@@ -0,0 +1,67 @@
+var adminUser = '',
+    adminPass = '';
+
+var unexpectedError = "An unexpected error occured. If you keep seeing this message, please reload the page or contact technical support.",
+    connectionError = "Could not connect. Please check your internet connection or try reloading the page.";
+
+var DomesticInt = 1,
+    InternationalInt = 2,
+    EconomicInt = 1,
+    BusinessInt = 2;
+    
+/*
+Description: Changes the current view
+Input: Class of the view to change to
+Output: None
+*/
+function changeView( viewClass ) {
+    $.when(
+        // Hide any currently active views
+        $('.View.Active').fadeOut()
+                         .removeClass('Active')
+    ).then( function() {
+        // Show the view being switched to
+        $('.View.' + viewClass ).fadeIn()
+                         .addClass('Active');
+    });
+}
+
+function login() {
+    adminUser = $('#loginUser')[0].value;
+    adminPass = $('#loginPass')[0].value;
+    
+    if( (adminUser.length < 1) || ( ($('#loginType')[0].value == 'SkyHigh') && (adminPass.length < 1) ) ){
+        alert('Please enter both a username and password.');
+        return;
+    }
+    
+    var request = {};
+    request.adminUser = adminUser;
+    request.adminPass = adminPass;
+    
+    if($('#loginType')[0].value == 'SkyHigh'){
+        // normal login
+    } else {
+        request.action = 'setupServer';
+        request = JSON.stringify(request);
+        $.post( "admin.php", { request: request }, function( response ) {
+            try {
+                response = jQuery.parseJSON(response);
+            } catch(e) {
+                response = {};
+            }
+            
+            if(response.error){
+                alert(response.error);
+            } else if(response.results){
+                $('.resultsFrame').html(response.results);
+                changeView('SetupResults');
+            } else {
+                alert(unexpectedError);
+            }
+        })
+        .fail(function(){
+            alert(connectionError);
+        });
+    }
+}
---
+++ admin.php
@@ -0,0 +1,121 @@
+<?php
+include 'common.php';
+
+$USER_ADMIN = 'SkyHighWebAppAdmin';
+$PASS_ADMIN = 'dwfmwG2zrWP4twk8';
+
+$AdminSaltConstant = "AdminPortalPassword";
+
+$missingParametersError = '{"error":"Your request is missing one or more parameters."}';
+$SQLerror = '{"error":"SQL statement execution failed. View the error log for more details."}';
+
+if(!isset($_POST["request"])){
+    die('{"error":"Request string missing or empty."}');
+}
+
+$request = json_decode($_POST['request']);
+if(!$request){
+    die('{"error":"The request could not be parsed."}');
+}
+
+if( !isset($request->action) || !isset($request->adminUser) || !isset($request->adminPass) ){
+    die($missingParametersError);
+}
+
+$response = new stdClass();
+
+switch ($request->action)
+{
+case "setupServer":
+    $connection = mysql_connect($DB_HOST, $request->adminUser, $request->adminPass);
+    if (!$connection) {
+        die('{"error":"Could not connect to MySQL server."}');
+    }
+
+    $db_selected = mysql_select_db($DB_NAME, $connection);
+
+    if (!$db_selected) {
+      $sql = 'CREATE DATABASE '.$DB_NAME;
+
+      if (mysql_query($sql, $connection)) {
+          $response->results = "Created skyhigh database";
+      } else {
+          die($SQLerror);
+      }
+    } else {
+        $response->results = "Database skyhigh found";
+    }
+    
+    mysql_close($connection);
+
+    try {
+        $connection = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $request->adminUser, $request->adminPass);
+
+        $statement = $connection->prepare("CREATE TABLE IF NOT EXISTS `bookedflights` (
+                                          `BookingID` int(11) NOT NULL,
+                                          `Username` varchar(100) NOT NULL,
+                                          `Date` varchar(10) NOT NULL,
+                                          `Adults` int(11) NOT NULL,
+                                          `Children` int(11) NOT NULL,
+                                          `Infants` int(11) NOT NULL,
+                                          `RateID` int(11) NOT NULL,
+                                          PRIMARY KEY (`BookingID`)
+                                        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+        $statement->execute();
+        $response->results = $response->results."<br><br>Created table for booked flights";
+        
+        $statement = $connection->prepare("CREATE TABLE IF NOT EXISTS `rates` (
+                                          `ID` int(11) NOT NULL,
+                                          `Type` int(11) NOT NULL,
+                                          `Class` int(11) NOT NULL,
+                                          `From` varchar(50) NOT NULL,
+                                          `To` varchar(50) NOT NULL,
+                                          `Price` int(11) NOT NULL,
+                                          `Time` time NOT NULL,
+                                          PRIMARY KEY (`ID`)
+                                        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+        $statement->execute();
+        $response->results = $response->results."<br><br>Created table for rates";
+        
+        $statement = $connection->prepare("CREATE TABLE IF NOT EXISTS `users` (
+                                          `Username` varchar(100) NOT NULL,
+                                          `Password` varchar(40) NOT NULL,
+                                          PRIMARY KEY (`Username`)
+                                        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+        $statement->execute();
+        $response->results = $response->results."<br><br>Created table for users";
+        
+        $defaultUser = "SkyHighAdmin";
+        $defaultPass = "password";
+        $defaultPassHashed = sha1($defaultPass.$defaultUser.$PassSaltConstant.$AdminSaltConstant);
+        
+        $statement = $connection->prepare("INSERT INTO admins (Username,Password) VALUES (:user,:pass)");
+        $statement->bindParam(':user', $defaultUser);
+        $statement->bindParam(':pass', $defaultPassHashed);
+        $statement->execute();
+        $response->results = $response->results."<br><br>Added default admin account to admins";
+        
+        $statement = $connection->prepare("GRANT SELECT ON *.* TO '".$USER_SELECT."'@'localhost' IDENTIFIED BY '".$PASS_SELECT."';");
+        $statement->execute();
+        $response->results = $response->results."<br><br>Added user for SELECT";
+        
+        $statement = $connection->prepare("GRANT SELECT, INSERT ON *.* TO '".$USER_INSERT."'@'localhost' IDENTIFIED BY '".$PASS_INSERT."';");
+        $statement->execute();
+        $response->results = $response->results."<br><br>Added user for INSERT";
+        
+        $statement = $connection->prepare("GRANT DELETE ON *.* TO '".$USER_DELETE."'@'localhost' IDENTIFIED BY '".$PASS_DELETE."';");
+        $statement->execute();
+        $response->results = $response->results."<br><br>Added user for DELETE";
+        
+        $connection = null;
+    } catch(PDOException $e) {
+        error_log($e->getMessage());
+        die($SQLerror);
+    }
+    break;
+default:
+  die('{"error":"Invalid action."}');
+}
+
+echo json_encode($response);
+?>
